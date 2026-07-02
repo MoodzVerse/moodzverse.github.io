d2a2b3b6ed60bf3e030e7331d6e902e9285aea2e
MoodzVerse <moodzbystar@hotmail.com>
2026-07-01 19:35:47 -0500

    Add typing effect for welcome message

--- js/main.js
+++ js/main.js
@@ -1,1 +1,19 @@
+const message = "Hi! I'm Starry. Welcome to Moodz Lounge.";
+const typedText = document.getElementById("typedText");
+const menu = document.getElementById("menu");
 
+let i = 0;
+
+function typeText() {
+  if (i < message.length) {
+    typedText.textContent += message[i];
+    i++;
+    setTimeout(typeText, 70);
+  } else {
+    setTimeout(() => {
+      menu.classList.remove("hidden");
+    }, 600);
+  }
+}
+
+typeText();
